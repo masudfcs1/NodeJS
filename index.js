88beae487e3b757b13cce59dd033b657a836b6ba
masudfcs1 <masudfcs1@gmail.com>
2022-08-27 19:40:53 +0600

    add line

---
+++ index.js
@@ -0,0 +1,5 @@
+const s1 = require('./student');
+
+console.log(s1.name());
+console.log(s1.age());
+console.log(s1.add());
---
+++ student.js
@@ -0,0 +1,15 @@
+const getname = () => {
+    return "Masud Rana";
+}
+
+const getAge = () => {
+    return "23";
+}
+
+const address = () => {
+    return "Manikgonj"
+}
+
+exports.name = getname;
+exports.age = getAge;
+exports.add = address;

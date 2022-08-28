1c96ac7d66177b1f005e79d78ec3bae8c53375a2
masudfcs1 <masudfcs1@gmail.com>
2022-08-28 14:50:52 +0600

    add line

--- Fs-Module/index.js
+++ Fs-Module/index.js
@@ -1,9 +1,31 @@
+const { log } = require('console');
 const fs = require('fs');
 
-fs.appendFile('./text.txt', ' I am 23 Year Old ', (error) => {
+// WirteFile 
+
+// fs.writeFile('./text.txt', 'This is Masud Rana', (error) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Sucessful');
+//     }
+// })
+
+
+// appendFile
+// fs.appendFile('./text.txt', ' I am 23 Year Old ', (error) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Sucessful');
+//     }
+// })
+
+
+fs.readFile('./text.txt', 'utf-8', (error, data) => {
     if (error) {
         console.log(error);
     } else {
-        console.log('Sucessful');
+        console.log('Sucessful', data);
     }
 })

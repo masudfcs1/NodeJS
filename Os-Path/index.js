397c235f8bad5bb2548df73a3ae9e670701a3f19
masudfcs1 <masudfcs1@gmail.com>
2022-08-28 20:11:59 +0600

    Os

--- Os-Path/index.js
+++ Os-Path/index.js
@@ -1,3 +1,10 @@
 const os = require('os')
 
-console.log(os.userInfo());
+//console.log(os.userInfo());
+// console.log(os.homedir());
+// console.log(os.EOL());
+// console.log(os.arch());
+
+// console.log(os.constants);
+
+console.log(os.cpus());

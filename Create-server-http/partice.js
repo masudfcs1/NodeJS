869a8df8195db37ed9d077aaa69dba2b58f497bc
masudfcs1 <masudfcs1@gmail.com>
2022-08-29 08:41:12 +0600

    with prot, hostname

---
+++ Create-server-http/partice.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const prot = 2000;
+const hostname = '127.0.0.1'
+
+
+const myserver = http.createServer((req, res) => {
+    res.end("Hi, Server is sucessfully Run")
+})
+
+
+myserver.listen(prot, hostname, () => {
+    console.log(`Server is Sucessfully Running at http://${hostname}:${prot}`);
+})

2104fc764d6a6d1dcb1136686975fca25ad9419e
masudfcs1 <masudfcs1@gmail.com>
2022-08-29 11:37:46 +0600

    npm

--- external-Module/index.js
+++ external-Module/index.js
@@ -1,4 +1,12 @@
-var moviesNames = require('movies-names');
-moviesNames.random()
+// var moviesNames = require('movies-names');
+// moviesNames.random()
 
-console.log(moviesNames.random());
+// console.log(moviesNames.random());
+
+// // console.log(moviesNames.getmovieName('The Dark Knight'));
+
+
+const computerName = require('computer-name')
+
+
+console.log(computerName());

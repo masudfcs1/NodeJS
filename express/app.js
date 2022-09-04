40a972392d88c918863c8fe82ea5ca3f12f9cc18
masudfcs1 <masudfcs1@gmail.com>
2022-09-04 08:40:44 +0600

    route

--- express/app.js
+++ express/app.js
@@ -3,6 +3,10 @@
  const port = process.env.port || 8080
  const hostname = '127.0.0.1'
 
+ app.get('/', (req, res) => {
+     res.send("<h1>Never Give Up!</h1>")
+ })
+
  app.listen(port, () => {
      console.log(`Sucessfully.Server is Run ${port}`);
  })

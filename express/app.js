a00c5f25f2b691431ba45016c2cb8daac9a1d9fd
masudfcs1 <masudfcs1@gmail.com>
2022-09-04 08:33:06 +0600

    express

---
+++ express/app.js
@@ -0,0 +1,11 @@
+ const express = require('express')
+ const app = express();
+ const port = process.env.port || 8080
+ const hostname =
+
+     app.listen(port, () => {
+         console.log(`Sucessfully.Server is Run ${port}`);
+     })
+
+
+ //  console.log('Masud Rana');

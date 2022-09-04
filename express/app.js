4123cec2c91b1866f55061d12a14828932d4ed23
masudfcs1 <masudfcs1@gmail.com>
2022-09-04 08:35:35 +0600

    add line

--- express/app.js
+++ express/app.js
@@ -1,11 +1,11 @@
  const express = require('express')
  const app = express();
  const port = process.env.port || 8080
- const hostname =
+ const hostname = '127.0.0.1'
 
-     app.listen(port, () => {
-         console.log(`Sucessfully.Server is Run ${port}`);
-     })
+ app.listen(port, () => {
+     console.log(`Sucessfully.Server is Run ${port}`);
+ })
 
 
  //  console.log('Masud Rana');

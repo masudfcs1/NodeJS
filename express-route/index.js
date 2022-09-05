9db20fb3679ffa77fe3ffdf104c4b8e2fd29ae23
masudfcs1 <masudfcs1@gmail.com>
2022-09-05 07:52:09 +0600

    add routing

--- express-route/index.js
+++ express-route/index.js
@@ -8,9 +8,33 @@ app.use(morgan('dev'))
 
 const router = express.Router()
 
+router.get('/login', (req, res) => {
+    res.send("<h1>This is Login Route.</h1>")
+    req.url
+
+})
 
 
+router.get('/signup', (req, res) => {
+    res.send("<h1>This is Login Route.</h1>")
+})
 
+
+router.get('/about', (req, res) => {
+    res.send("<h1>This is About Route.</h1>")
+})
+
+
+router.get('/contact', (req, res) => {
+    res.send("<h1>This is Contact Route.</h1>")
+})
+
+app.use("/user", router)
+
+
+app.get("/", (req, res) => {
+    res.send('<h1>Hi,There</h1>')
+})
 
 
 app.listen(port, () => {

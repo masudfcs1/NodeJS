19549e4623e34ba5537fc3a2bb56829864c9f3ea
masudfcs1 <masudfcs1@gmail.com>
2022-09-08 06:38:34 +0600

    created server

--- mini-project/index.js
+++ mini-project/index.js
@@ -1,2 +1,8 @@
 const express = require('express')
 const app = express()
+
+const prot = process.env.prot || 1010
+
+app.listen(prot, () => {
+    console.log(`Sever is Running on port ${prot}`);
+})

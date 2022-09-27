fdab5efc0da7a5cf1a9f4c43d3d5f287a30eb840
masudfcs1 <masudfcs1@gmail.com>
2022-09-27 09:10:52 +0600

    add line

--- template-Engine/app.js
+++ template-Engine/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const app = express()
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { title: 'Title EJS Apply' })
 })
 
 app.use(morgan('dev'))

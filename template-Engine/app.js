fae85a5923e96f333e26537e8bb17d598525ce34
masudfcs1 <masudfcs1@gmail.com>
2022-09-27 08:55:33 +0600

    add line

--- template-Engine/app.js
+++ template-Engine/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     res.render('index')
 })

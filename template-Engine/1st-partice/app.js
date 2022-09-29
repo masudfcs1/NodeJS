291caab531c90bc708793a8210970d948de26216
masudfcs1 <masudfcs1@gmail.com>
2022-09-29 11:14:19 +0600

    template-engine-EJS

--- template-Engine/1st-partice/app.js
+++ template-Engine/1st-partice/app.js
@@ -15,9 +15,22 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     // res.send('Sucessfully Started!')   
+
+    // let posts=[
+    //     {title: 'title One',author:"Masud Rana"},
+    //     {title: 'title two',author:"Masud Rana"},
+    //     {title: 'title three',author:"Masud Rana"},
+    //     {title: 'title Four',author:"Masud Rana"}
+
+    // ]
+    //  res.render('index',{title:'EJS is an awesome',posts})
+
     res.render('index')
+    
 })
+const http=require('http')
+const hostname='127.0.0.1'
 
 app.listen(port, () => {
-    console.log(`Successfully Running on port ${port}`);
+    console.log(`Successfully Running on port http://${hostname}:${port}`);
 })

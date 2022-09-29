b77343f0fec623c171173162ee93d5adbce927a8
masudfcs1 <masudfcs1@gmail.com>
2022-09-29 13:04:42 +0600

    Multipule-Pages

---
+++ template-Engine/Multipule-pages/app.js
@@ -0,0 +1,26 @@
+const express =require('express')
+const morgan =require('morgan')
+const http =require('http')
+
+const app =express()
+
+app.set('view engine','ejs')
+
+app.use(morgan('dev'))
+
+app. get('/',(req,res)=>{
+    res.render ('pages/index')
+})
+app. get('/about',(req,res)=>{
+    res.render ('pages/about')
+})
+app. get('/service',(req,res)=>{
+    res.render ('pages/service')
+})
+
+const port=3030
+const hostname='127.0.0.1'
+
+app.listen(port,hostname,()=>{
+    console.log(`Server is Sucessfully Running at http://${hostname}:${port}`);
+})

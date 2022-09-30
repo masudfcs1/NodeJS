0cb8665c533fe692dcf79bf321ef0f103e2cf16d
masudfcs1 <masudfcs1@gmail.com>
2022-09-30 18:21:10 +0600

    Database

---
+++ Data.Base/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const morgan = require('morgan')
+const port = process.env.port || 8080
+const http=require('http')
+const hostname='127.0.0.1'
+const mongoose=require('mongoose')
+
+const app = express()
+
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+           message: 'Hello there!'
+    })
+})
+
+mongoose.connect('mongodb://localhost:27017',{
+    useNewUrlParser: false
+})
+
+.then(()=>{
+    app.listen(port,hostname, () => {
+  //  console.log(`Server is Running at http://${hostname}:${port}`);
+    console.log(`Server is Sucessfully Running at http://${hostname}:${port}`);
+   })
+})
+
+.catch(e =>{
+   console.log(e);
+})
+
+
